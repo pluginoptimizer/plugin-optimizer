68a412815ba5887e2c1cdd3235d51e0d17236161
Academ <academdevelopers@gmail.com>
2021-01-12 21:14:14 +0200

    Hotfix.

--- admin/class-plugin-optimizer-admin.php
+++ admin/class-plugin-optimizer-admin.php
@@ -1376,15 +1376,15 @@ class Plugin_Optimizer_Admin {
 												?>
                                                 <div class="content <?= in_array( $group->post_title, $groups_plugins ) ? 'block' : ''; ?> ">
                                                     <span><?= $group->post_title; ?></span>
-													<? $block_plugins_in_group = explode( ',', get_post_meta( $group->ID, 'group_plugins', true ) );
+													<?/* $block_plugins_in_group = explode( ',', get_post_meta( $group->ID, 'group_plugins', true ) );
 													foreach ( $block_plugins_in_group as $block_plugin_in_group ) :
-														?>
+														*/?><!--
                                                         <div class="hidden_content content">
-                                                            <span><?= $block_plugin_in_group; ?></span>
+                                                            <span><?/*= $block_plugin_in_group; */?></span>
                                                         </div>
-													<?php
-													endforeach;
-													?>
+													--><?php
+/*													endforeach;
+													*/?>
                                                 </div>
 											<?php
 											endforeach;
--- admin/partials/page-filters.php
+++ admin/partials/page-filters.php
@@ -169,15 +169,15 @@
 														?>
                                                         <div class="content">
                                                             <span><?= $group->post_title; ?></span>
-															<? $block_plugins_in_group = explode( ',', get_post_meta( $group->ID, 'group_plugins', true ) );
+															<?/* $block_plugins_in_group = explode( ',', get_post_meta( $group->ID, 'group_plugins', true ) );
 															foreach ( $block_plugins_in_group as $block_plugin_in_group ) :
-																?>
+																*/?><!--
                                                                 <div class="hidden_content content">
-                                                                    <span><?= $block_plugin_in_group; ?></span>
+                                                                    <span><?/*= $block_plugin_in_group; */?></span>
                                                                 </div>
-															<?php
-															endforeach;
-															?>
+															--><?php
+/*															endforeach;
+															*/?>
                                                         </div>
 													<?php
 													endforeach;
