0ffda62da435ece6283647e9453e8cca3afa0559
Academ <academdevelopers@gmail.com>
2021-01-28 20:47:44 +0200

    Valid links check.

--- admin/js/components/choice-link.js
+++ admin/js/components/choice-link.js
@@ -8,12 +8,33 @@ let choiceLinks;
         /*     Show the selected links    */
         choiceLinks = () => {
 
+            function get_hostname(url) {
+                let m = url.match(/^http:\/\/[^/]+/);
 
+                if(m){
+                    return m[0];
+                }
+                m = url.match(/^https:\/\/[^/]+/);
+                return m ? m[0] : null;
+            }
+
+            $('body').on('keypress', '#search_pages', function(e){
+                if (e.keyCode == 13) {
+                    $(`.add-permalink`).click();
+                }
+            });
 
             $(`.add-permalink`).click(function () {
+                let linkClient = $(`#search_pages`).val();
+                linkClient = get_hostname(linkClient) ? linkClient.replace(get_hostname(linkClient), '') : linkClient;
+                linkClient = linkClient.indexOf('/') === 0 ? linkClient.replace('/', '') : linkClient;
+                linkClient = linkClient.includes(location.hostname) ? linkClient : `${location.hostname}/${linkClient}`;
+                linkClient = linkClient.includes(`https://`) ? linkClient : `https://${linkClient}`;
+
+
                 $(this).parent().after(`
             <div class="link">
-                <span class="data-interaction text_link" contenteditable>${$(`#search_pages`).val()}</span>
+                <span class="data-interaction text_link" contenteditable>${linkClient}</span>
                 <span class="close-selected-link">×</span>
             </div>
             `);
