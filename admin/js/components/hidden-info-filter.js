2561059a60dde6f6f423af364af8ab23bd2990da
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-02-14 02:57:58 +0100

    Fixed a .js backtick issue

--- admin/js/components/hidden-info-filter.js
+++ admin/js/components/hidden-info-filter.js
@@ -1,17 +1,18 @@
 let hiddenInfoFilter;
 (function ($) {
     'use strict';
-
+    
     $(document).ready(function () {
+        
         // hidden info filter
         hiddenInfoFilter = () =>{
             $('.block_info > td:not(:nth-child(1))').click(function () {
                 const element_id = $(this).parent().children('td:nth-child(1)').children().attr('id');
 
                 if($('#name_page').attr("class") === 'filters_categories'){
-                    location.href='/wp-admin/term.php?taxonomy&tag_ID=${element_id}&post_type=sos_filter';
+                    location.href=`/wp-admin/term.php?taxonomy&tag_ID=${element_id}&post_type=sos_filter`;
                 } else {
-                    location.href='/wp-admin/post.php?post=${element_id}&action=edit';
+                    location.href=`/wp-admin/post.php?post=${element_id}&action=edit`;
                 }
 
                 /*if($(this).parent().next('.hidden_info').css('display') === 'none'){
