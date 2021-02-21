5519897d784bf9b3dbd49401d73e5d3df91363d4
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-02-21 16:35:53 +0100

    Changed the version and the author

--- plugin-optimizer.php
+++ plugin-optimizer.php
@@ -7,9 +7,9 @@
  * Plugin Name:       Plugin Optimizer
  * Plugin URI:        plugin-uri.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Web Dev
- * Author URI:        author-uri.com
+ * Version:           0.0.2
+ * Author:            Simple Online Systems
+ * Author URI:        https://simpleonlinesystems.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-optimizer
