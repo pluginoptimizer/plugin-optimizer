7857489da10bedfeeedc1b94b4acc95f2e364828
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-03-09 14:09:27 +0100

    Minor change

--- includes/class-po-mu.php
+++ includes/class-po-mu.php
@@ -4,7 +4,7 @@
  * Plugin URI:        plugin-uri.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Web Dev
+ * Author:            Simple Online Systems
  * Author URI:        author-uri.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
