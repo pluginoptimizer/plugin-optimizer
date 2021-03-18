b3e0ac68ad2296d1d87aeda60c511cda768570ba
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-03-18 14:18:18 +0100

    Fixed: Please use wp_enqueue commands

--- admin/class-po-admin.php
+++ admin/class-po-admin.php
@@ -51,8 +51,6 @@ class SOSPO_Admin {
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_styles'          ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts'         ] );
         
-		add_filter( 'script_loader_tag',     [ $this, 'add_type_attribute'      ], 10, 3 );
-        
 		add_action( 'init',                  [ $this, 'register_post_types'     ] );
 		add_action( 'init',                  [ $this, 'register_taxonomies'     ] );
         
@@ -149,20 +147,6 @@ class SOSPO_Admin {
 		wp_localize_script( $this->plugin_name, 'po_object', $array );
 		wp_enqueue_script(  $this->plugin_name );
 
-	}
-
-	/**
-	 * Add type module for js
-	 */
-	function add_type_attribute( $tag, $handle, $src ) {
-		// if not your script, do nothing and return original $tag
-		if ( $this->plugin_name !== $handle ) {
-			return $tag;
-		}
-		// change the script tag by adding type="module" and return it.
-		$tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
-
-		return $tag;
 	}
 
 	function check_memory_usage() {
