cfaa5e95f4f74084ed5829ceaeda83dd9d20cecd
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-03-28 17:27:51 +0200

    Bumped up the version to 1.0.5

--- includes/class-po.php
+++ includes/class-po.php
@@ -43,7 +43,7 @@ class PluginOptimizer {
 		if ( defined( 'SOSPO_VERSION' ) ) {
 			$this->version = SOSPO_VERSION;
 		} else {
-			$this->version = '1.0.4';
+			$this->version = '1.0.5';
 		}
 		$this->plugin_name = 'plugin-optimizer';
 
