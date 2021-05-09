d6dae9fc22e7f04ab19f768769b26f6c310baf68
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-05-09 02:20:12 +0200

    Some columns need to be used only on the Agent sites

--- admin/pages/page-filters-list.php
+++ admin/pages/page-filters-list.php
@@ -45,17 +45,19 @@ if( $filters ){
                 <table class="po_table">
                     <thead>
                     
-                    <?php if( sospo_mu_plugin()->has_agent ){ ?>
-                    <?php } ?>
                         <tr id="search_boxes" class="toggle_filter_options hidden">
                             <th></th>
                             <th data-label="title" class="align-left"><input type="text" placeholder="Search Title..." class="search_filter"/></th>
                             <th data-label="categories" class="align-left"><input type="text" placeholder="Search Categories..." class="search_filter"/></th>
                             <th data-label="triggers"><input type="text" placeholder="Search Triggers..." class="search_filter"/></th>
+                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
                             <th data-label="belongs_to"><input type="text" placeholder="Search Belongs To..." class="search_filter"/></th>
+                        <?php } ?>
                             <th data-label="plugins_tooltip"><input type="text" placeholder="Search Plugins..." class="search_filter"/></th>
+                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
                             <th></th>
                             <th></th>
+                        <?php } ?>
                             <th class="toggle_filter"></th>
                         </tr>
                     
@@ -64,10 +66,14 @@ if( $filters ){
                             <th data-label="title" class="left-10 align-left sort_able sort_active">Title</th>
                             <th class="left-10 align-left">Categories</th>
                             <th>Triggers</th>
+                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
                             <th>Belongs to</th>
+                        <?php } ?>
                             <th data-label="plugins_tooltip" class="sort_able">Blocked plugins</th>
+                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
                             <th data-label="created" class="sort_able">Created</th>
                             <th data-label="modified" class="sort_able">Modified</th>
+                        <?php } ?>
                             <th class="toggle_filter">Turned On</th>
                         </tr>
                         
