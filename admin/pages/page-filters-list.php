f21da862055907dc1145a46999b90633f2676984
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-05-13 16:37:18 +0200

    possible fix for broken table header on filters list page

--- admin/pages/page-filters-list.php
+++ admin/pages/page-filters-list.php
@@ -28,10 +28,10 @@ if( $filters ){
                 
                 <?php SOSPO_Admin_Helper::content_part__manipulate_filter_options(); ?>
                 
-                <div id="show_toggle_columns" class="toggle_columns">
+                <div id="show_toggle_columns" class="toggle_columns" style="display: none;">
                     Show Toggle Columns
                 </div>
-                <div id="hide_toggle_columns" class="toggle_columns">
+                <div id="hide_toggle_columns" class="toggle_columns" style="display: none;">
                     Hide Toggle Columns
                 </div>
                 
@@ -47,55 +47,53 @@ if( $filters ){
         
         <?php SOSPO_Admin_Helper::content_part__filter_options( $filters ); ?>
         
-        <div class="row col-12">
-            <div class="col-12">
-                <table class="po_table">
-                    <thead>
+        <div>
+            <table class="po_table">
+                <thead>
+                
+                    <tr id="search_boxes" class="toggle_filter_options hidden">
+                        <th data-label="checkbox"></th>
+                    <?php if( sospo_mu_plugin()->has_agent ){ ?>
+                        <th data-label="status"></th>
+                    <?php } ?>
+                        <th data-label="title" class="align-left"><input type="text" placeholder="Search Title..." class="search_filter"/></th>
+                        <th data-label="categories" class="align-left"><input type="text" placeholder="Search Categories..." class="search_filter"/></th>
+                        <th data-label="triggers"><input type="text" placeholder="Search Triggers..." class="search_filter"/></th>
+                    <?php if( sospo_mu_plugin()->has_agent ){ ?>
+                        <th data-label="belongs_to"><input type="text" placeholder="Search Belongs To..." class="search_filter"/></th>
+                    <?php } ?>
+                        <th data-label="plugins_tooltip"><input type="text" placeholder="Search Plugins..." class="search_filter"/></th>
+                    <?php if( sospo_mu_plugin()->has_agent ){ ?>
+                        <th data-label="created"></th>
+                        <th data-label="modified"></th>
+                    <?php } ?>
+                        <th class="toggle_filter"></th>
+                    </tr>
+                
+                    <tr id="po_table_header">
+                        <th data-label="checkbox"><input type="checkbox" id="check_all"></th>
+                    <?php if( sospo_mu_plugin()->has_agent ){ ?>
+                        <th data-label="status">Status</th>
+                    <?php } ?>
+                        <th data-label="title" class="left-10 align-left sort_able sort_active">Title</th>
+                        <th data-label="categories"class="left-10 align-left">Categories</th>
+                        <th data-label="triggers">Triggers</th>
+                    <?php if( sospo_mu_plugin()->has_agent ){ ?>
+                        <th data-label="belongs_to">Belongs to</th>
+                    <?php } ?>
+                        <th data-label="plugins_tooltip" class="sort_able">Blocking</th>
+                    <?php if( sospo_mu_plugin()->has_agent ){ ?>
+                        <th data-label="created" class="sort_able">Created</th>
+                        <th data-label="modified" class="sort_able">Modified</th>
+                    <?php } ?>
+                        <th class="toggle_filter">Turned On</th>
+                    </tr>
                     
-                        <tr id="search_boxes" class="toggle_filter_options hidden">
-                            <th data-label="checkbox"></th>
-                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
-                            <th data-label="status"></th>
-                        <?php } ?>
-                            <th data-label="title" class="align-left"><input type="text" placeholder="Search Title..." class="search_filter"/></th>
-                            <th data-label="categories" class="align-left"><input type="text" placeholder="Search Categories..." class="search_filter"/></th>
-                            <th data-label="triggers"><input type="text" placeholder="Search Triggers..." class="search_filter"/></th>
-                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
-                            <th data-label="belongs_to"><input type="text" placeholder="Search Belongs To..." class="search_filter"/></th>
-                        <?php } ?>
-                            <th data-label="plugins_tooltip"><input type="text" placeholder="Search Plugins..." class="search_filter"/></th>
-                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
-                            <th data-label="created"></th>
-                            <th data-label="modified"></th>
-                        <?php } ?>
-                            <th class="toggle_filter"></th>
-                        </tr>
-                    
-                        <tr id="po_table_header">
-                            <th data-label="checkbox"><input type="checkbox" id="check_all"></th>
-                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
-                            <th data-label="status">Status</th>
-                        <?php } ?>
-                            <th data-label="title" class="left-10 align-left sort_able sort_active">Title</th>
-                            <th data-label="categories"class="left-10 align-left">Categories</th>
-                            <th data-label="triggers">Triggers</th>
-                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
-                            <th data-label="belongs_to">Belongs to</th>
-                        <?php } ?>
-                            <th data-label="plugins_tooltip" class="sort_able">Blocking</th>
-                        <?php if( sospo_mu_plugin()->has_agent ){ ?>
-                            <th data-label="created" class="sort_able">Created</th>
-                            <th data-label="modified" class="sort_able">Modified</th>
-                        <?php } ?>
-                            <th class="toggle_filter">Turned On</th>
-                        </tr>
-                        
-                    </thead>
-                    <tbody id="the-list" class="filter_on__status_publish">
-                        <?php SOSPO_Admin_Helper::list_content__filters( $filters ); ?>
-                    </tbody>
-                </table>
-            </div>
+                </thead>
+                <tbody id="the-list" class="filter_on__status_publish">
+                    <?php SOSPO_Admin_Helper::list_content__filters( $filters ); ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
