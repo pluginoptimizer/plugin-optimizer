3a13ae0ccd9b18cf4bdc2d92774410de5fdf6c52
Andrija Naglic <andrija@simpleonlinesystems.com>
2021-05-15 03:25:24 +0200

    Added first set of methods

--- admin/pages/page-filters-list.php
+++ admin/pages/page-filters-list.php
@@ -10,6 +10,14 @@ if( $filters ){
     usort( $filters, "SOSPO_Admin_Helper::sort__by_post_title" );
 }
 
+// $relevant_filters = sospo_dictionary()->get_relevant_filters();
+// $pending_filters  = sospo_dictionary()->get_pending_filters();
+// $approved_filters = sospo_dictionary()->get_approved_filters();
+
+// sospo_mu_plugin()->write_log( $relevant_filters, "page_filters_list-relevant_filters" );
+// sospo_mu_plugin()->write_log( $pending_filters,  "page_filters_list-pending_filters" );
+// sospo_mu_plugin()->write_log( $approved_filters, "page_filters_list-approved_filters" );
+
 ?>
 
 <div class="sos-wrap">
