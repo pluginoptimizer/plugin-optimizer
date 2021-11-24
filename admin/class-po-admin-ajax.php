377c4a1906e2286c790665df7b55b212fd0de7f7
Jake Rodriguez <jake@simpleonlinesystems.com>
2021-11-24 14:37:39 -0800

    removing categories are not saving in database

--- admin/class-po-admin-ajax.php
+++ admin/class-po-admin-ajax.php
@@ -9,47 +9,47 @@
 
 class SOSPO_Ajax {
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @param string $plugin_name The name of this plugin.
-	 * @param string $version The version of this plugin.
-	 */
-	function __construct() {
+  /**
+   * Initialize the class and set its properties.
+   *
+   * @param string $plugin_name The name of this plugin.
+   * @param string $version The version of this plugin.
+   */
+  function __construct() {
 
-        $this->load_hooks();
-	}
+      $this->load_hooks();
+  }
 
-	/**
-	 * Register all of the hooks related to the admin area functionality of the plugin.
-	 *
-	 * @access   private
-	 */
-	function load_hooks() {
+  /**
+   * Register all of the hooks related to the admin area functionality of the plugin.
+   *
+   * @access   private
+   */
+  function load_hooks() {
 
-		add_action( 'wp_ajax_po_save_filter',                   [ $this, 'po_save_filter'                   ] );
-		add_action( 'wp_ajax_po_save_group',                    [ $this, 'po_save_group'                    ] );
-		add_action( 'wp_ajax_po_save_category',                 [ $this, 'po_save_category'                 ] );
-		add_action( 'wp_ajax_po_create_category',               [ $this, 'po_create_category'               ] );
-		add_action( 'wp_ajax_po_delete_elements',               [ $this, 'po_delete_elements'               ] );
-		add_action( 'wp_ajax_po_publish_elements',              [ $this, 'po_publish_elements'              ] );
-		add_action( 'wp_ajax_po_turn_filter_on',                [ $this, 'po_turn_filter_on'                ] );
-		add_action( 'wp_ajax_po_turn_filter_off',               [ $this, 'po_turn_filter_off'               ] );
-		add_action( 'wp_ajax_po_mark_tab_complete',             [ $this, 'po_mark_tab_complete'             ] );
-		add_action( 'wp_ajax_po_save_option_alphabetize_menu',  [ $this, 'po_save_option_alphabetize_menu'  ] );
-		add_action( 'wp_ajax_po_turn_off_filter',               [ $this, 'po_turn_off_filter'               ] );
-		add_action( 'wp_ajax_po_save_original_menu',            [ $this, 'po_save_original_menu'            ] );
-		add_action( 'wp_ajax_po_get_post_types',                [ $this, 'po_get_post_types'                ] );
-    add_action( 'wp_ajax_po_scan_prospector',               [ $this, 'po_scan_prospector'               ] );
-    add_action( 'wp_ajax_po_save_columns_state',            [ $this, 'po_save_columns_state'            ] );
+      add_action( 'wp_ajax_po_save_filter',                   [ $this, 'po_save_filter'                   ] );
+      add_action( 'wp_ajax_po_save_group',                    [ $this, 'po_save_group'                    ] );
+      add_action( 'wp_ajax_po_save_category',                 [ $this, 'po_save_category'                 ] );
+      add_action( 'wp_ajax_po_create_category',               [ $this, 'po_create_category'               ] );
+      add_action( 'wp_ajax_po_delete_elements',               [ $this, 'po_delete_elements'               ] );
+      add_action( 'wp_ajax_po_publish_elements',              [ $this, 'po_publish_elements'              ] );
+      add_action( 'wp_ajax_po_turn_filter_on',                [ $this, 'po_turn_filter_on'                ] );
+      add_action( 'wp_ajax_po_turn_filter_off',               [ $this, 'po_turn_filter_off'               ] );
+      add_action( 'wp_ajax_po_mark_tab_complete',             [ $this, 'po_mark_tab_complete'             ] );
+      add_action( 'wp_ajax_po_save_option_alphabetize_menu',  [ $this, 'po_save_option_alphabetize_menu'  ] );
+      add_action( 'wp_ajax_po_turn_off_filter',               [ $this, 'po_turn_off_filter'               ] );
+      add_action( 'wp_ajax_po_save_original_menu',            [ $this, 'po_save_original_menu'            ] );
+      add_action( 'wp_ajax_po_get_post_types',                [ $this, 'po_get_post_types'                ] );
+      add_action( 'wp_ajax_po_scan_prospector',               [ $this, 'po_scan_prospector'               ] );
+      add_action( 'wp_ajax_po_save_columns_state',            [ $this, 'po_save_columns_state'            ] );
 
-	}
+  }
 
-	/**
-	 * Create/Update filter
-	 */
-	function po_save_filter() {
-        
+  /**
+   * Create/Update filter
+   */
+  function po_save_filter() {
+    
     if( empty( $_POST['data'] ) ){              wp_send_json_error( [ "message" => "The data never reached the server!" ] ); }
     
     parse_str( $_POST['data'], $array);
@@ -65,24 +65,23 @@ class SOSPO_Ajax {
         
         wp_send_json_error( [ "message" => $data->get_error_message() ] );
     }
+            
+    $post_data = array(
+        'post_title'  => $data["title"],
+        'post_type'   => 'plgnoptmzr_filter',
+        'post_status' => 'publish',
+        'post_author' => 1,// TODO get_current_user_id() with localize_script in enqueue function
+    );
     
-        
-		$post_data = array(
-			'post_title'  => $data["title"],
-			'post_type'   => 'plgnoptmzr_filter',
-			'post_status' => 'publish',
-			'post_author' => 1,// TODO get_current_user_id() with localize_script in enqueue function
-		);
-        
     if( ! empty( $data["ID"] ) ){
         $post_data["ID"] = $data["ID"];
     }
 
-		$post_id = wp_insert_post( $post_data, true );
+    $post_id = wp_insert_post( $post_data, true );
 
-		if ( is_wp_error( $post_id ) ) {
-			wp_send_json_error( [ "message" => $post_id->get_error_message() ] );
-		}
+    if ( is_wp_error( $post_id ) ) {
+        wp_send_json_error( [ "message" => $post_id->get_error_message() ] );
+    }
 
     // check if the agent plugin is installed
     if( in_array('plugin-optimizer-agent/plugin-optimizer-agent.php', get_option('active_plugins')) ){
@@ -91,10 +90,15 @@ class SOSPO_Ajax {
         update_post_meta( $post_id, 'premium_filter', 'true');
     }
 
+
+    // Let's clear the categories first.
+    $set_categories = wp_set_object_terms( $post_id, array(), "plgnoptmzr_categories" );
+
     if( ! empty( $data["meta"]["categories"] ) ){
         
         $category_ids = array_map( 'intval', array_keys( $data["meta"]["categories"] ) );
         
+        // if we have categories, then we resave them.
         $set_categories = wp_set_object_terms( $post_id, $category_ids, "plgnoptmzr_categories" );
     }
     
@@ -102,16 +106,16 @@ class SOSPO_Ajax {
         
         update_post_meta( $post_id, $meta_key, $meta_value );
     }
-        
-		wp_send_json_success( [ "message" => "All good, the filter is saved.", "id" => $post_id, ] );
+      
+    wp_send_json_success( [ "message" => "All good, the filter is saved.", "id" => $post_id, ] );
 
-	}
+  }
 
-	/**
-	 * Create/Update Group
-	 */
-	function po_save_group() {
-        
+  /**
+   * Create/Update Group
+   */
+  function po_save_group() {
+      
     if( empty( $_POST['data'] ) ){              wp_send_json_error( [ "message" => "The data never reached the server!" ] ); }
     
     parse_str( $_POST['data'], $array);
@@ -128,37 +132,37 @@ class SOSPO_Ajax {
         wp_send_json_error( [ "message" => $data->get_error_message() ] );
     }
         
-		$post_data = array(
-			'post_title'  => $data["title"],
-			'post_type'   => 'plgnoptmzr_group',
-			'post_status' => 'publish',
-			'post_author' => 1,// TODO get_current_user_id() with localize_script in enqueue function
-		);
+        $post_data = array(
+            'post_title'  => $data["title"],
+            'post_type'   => 'plgnoptmzr_group',
+            'post_status' => 'publish',
+            'post_author' => 1,// TODO get_current_user_id() with localize_script in enqueue function
+        );
     
     if( ! empty( $data["ID"] ) ){
         $post_data["ID"] = $data["ID"];
     }
 
-		$post_id = wp_insert_post( $post_data, true );
+        $post_id = wp_insert_post( $post_data, true );
 
-		if ( is_wp_error( $post_id ) ) {
-			wp_send_json_error( [ "message" => $post_id->get_error_message() ] );
-		}
+        if ( is_wp_error( $post_id ) ) {
+            wp_send_json_error( [ "message" => $post_id->get_error_message() ] );
+        }
 
     foreach( $data["meta"] as $meta_key => $meta_value ){
         
         update_post_meta( $post_id, $meta_key, $meta_value );
     }
-            
-		wp_send_json_success( [ "message" => "All good, the group is saved.", "id" => $post_id, ] );
+          
+      wp_send_json_success( [ "message" => "All good, the group is saved.", "id" => $post_id, ] );
 
-	}
+  }
 
-	/**
-	 * Create/Update Category
-	 */
-	function po_save_category() {
-        
+  /**
+   * Create/Update Category
+   */
+  function po_save_category() {
+      
     if( empty( $_POST['data'] ) ){              wp_send_json_error( [ "message" => "The data never reached the server!" ] ); }
     
     parse_str( $_POST['data'], $array);
@@ -196,16 +200,16 @@ class SOSPO_Ajax {
     if( is_wp_error( $category ) ){
         wp_send_json_error( [ "message" => "An error occured: " . $category->get_error_message() ] );
     }
-        
-		wp_send_json_success( [ "message" => "All good, the category is saved.", "id" => $category["term_id"], ] );
+      
+      wp_send_json_success( [ "message" => "All good, the category is saved.", "id" => $category["term_id"], ] );
 
-	}
+  }
 
-	/**
-	 * Create new category
-	 */
-	function po_create_category(){
-        
+  /**
+   * Create new category
+   */
+  function po_create_category(){
+      
     // sospo_mu_plugin()->write_log( $_POST, "po_create_category-_POST" );
     
     if( empty( $_POST['category_name'] ) ){
@@ -225,117 +229,117 @@ class SOSPO_Ajax {
     }
     
     wp_send_json_success( [ "category_id" => $data['term_id'] ] );
-	}
+  }
 
-	/**
-	 * Delete elements
-	 */
-	function po_delete_elements() {
-        
-		$name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
-		$type_elements  = sanitize_textarea_field( $_POST['type_elements'] );
-        $id_elements    = array_map( 'intval', $_POST['id_elements'] );
+  /**
+   * Delete elements
+   */
+  function po_delete_elements() {
+      
+      $name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
+      $type_elements  = sanitize_textarea_field( $_POST['type_elements'] );
+      $id_elements    = array_map( 'intval', $_POST['id_elements'] );
 
-		if ( $name_post_type === 'cat' ) {
-			
-			foreach ( $id_elements as $id_element ) {
-				wp_delete_term( $id_element, 'plgnoptmzr_categories' );
-			}
+      if ( $name_post_type === 'cat' ) {
+          
+          foreach ( $id_elements as $id_element ) {
+              wp_delete_term( $id_element, 'plgnoptmzr_categories' );
+          }
 
-			wp_send_json_success( [ "status" => "success", "message" => "Categories are deleted." ] );
-            
-		} elseif ( $type_elements === 'all' ) {
-         
-			foreach ( $id_elements as $post_id ) {
-				wp_trash_post( $post_id );
-			}
-            
-			wp_send_json_success( [ "status" => "success", "message" => "Items are moved to trash." ] );
-            
-		} else {
-            
-			$posts = get_posts( array(
-				'post_type'   => $name_post_type,
-				'include'     => $id_elements,
-				'post_status' => 'trash',
-			) );
+          wp_send_json_success( [ "status" => "success", "message" => "Categories are deleted." ] );
+          
+      } elseif ( $type_elements === 'all' ) {
+       
+          foreach ( $id_elements as $post_id ) {
+              wp_trash_post( $post_id );
+          }
+          
+          wp_send_json_success( [ "status" => "success", "message" => "Items are moved to trash." ] );
+          
+      } else {
+          
+          $posts = get_posts( array(
+              'post_type'   => $name_post_type,
+              'include'     => $id_elements,
+              'post_status' => 'trash',
+          ) );
 
-			foreach ( $posts as $post ) {
-				wp_delete_post( $post->ID, true );
-			}
-            
-			wp_send_json_success( [ "status" => "success", "message" => "Items are permanently deleted." ] );
-            
-		}
-	}
+          foreach ( $posts as $post ) {
+              wp_delete_post( $post->ID, true );
+          }
+          
+          wp_send_json_success( [ "status" => "success", "message" => "Items are permanently deleted." ] );
+          
+      }
+  }
 
-	/**
-	 * Restore works
-	 */
-	function po_publish_elements() {
-        
-		$name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
-        $id_elements    = array_map( 'intval', $_POST['id_elements'] );
+  /**
+   * Restore works
+   */
+  function po_publish_elements() {
+      
+      $name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
+      $id_elements    = array_map( 'intval', $_POST['id_elements'] );
 
-		$posts = get_posts( array(
-			'post_type'   => $name_post_type,
-			'include'     => $id_elements,
-			'post_status' => 'trash',
-		) );
+      $posts = get_posts( array(
+          'post_type'   => $name_post_type,
+          'include'     => $id_elements,
+          'post_status' => 'trash',
+      ) );
 
-		foreach ( $posts as $post ) {
-			wp_publish_post( $post->ID );
-		}
-        
-		wp_send_json_success( [ "message" => "Items are restored." ] );
-        
-	}
-    
-	/**
-	 * Bulk turn filters on
-	 */
-	function po_turn_filter_on() {
-        
-		$name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
-        $id_elements    = array_map( 'intval', $_POST['id_elements'] );
+      foreach ( $posts as $post ) {
+          wp_publish_post( $post->ID );
+      }
+      
+      wp_send_json_success( [ "message" => "Items are restored." ] );
+      
+  }
+  
+  /**
+   * Bulk turn filters on
+   */
+  function po_turn_filter_on() {
+      
+      $name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
+      $id_elements    = array_map( 'intval', $_POST['id_elements'] );
 
-		$posts = get_posts( array(
-			'post_type'   => $name_post_type,
-			'include'     => $id_elements,
-		) );
+      $posts = get_posts( array(
+          'post_type'   => $name_post_type,
+          'include'     => $id_elements,
+      ) );
 
-		foreach ( $posts as $post ) {
-			update_post_meta( $post->ID, "turned_off", false );
-		}
-        
-		wp_send_json_success( [ "message" => count( $posts) . " filters are turned on." ] );
-        
-	}
-    
-	/**
-	 * Bulk turn filters off
-	 */
-	function po_turn_filter_off() {
-        
-		$name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
-        $id_elements    = array_map( 'intval', $_POST['id_elements'] );
+      foreach ( $posts as $post ) {
+          update_post_meta( $post->ID, "turned_off", false );
+      }
+      
+      wp_send_json_success( [ "message" => count( $posts) . " filters are turned on." ] );
+      
+  }
+  
+  /**
+   * Bulk turn filters off
+   */
+  function po_turn_filter_off() {
+      
+      $name_post_type = sanitize_textarea_field( $_POST['name_post_type'] );
+      $id_elements    = array_map( 'intval', $_POST['id_elements'] );
 
-		$posts = get_posts( array(
-			'post_type'   => $name_post_type,
-			'include'     => $id_elements,
-		) );
+      $posts = get_posts( array(
+          'post_type'   => $name_post_type,
+          'include'     => $id_elements,
+      ) );
 
-		foreach ( $posts as $post ) {
-			update_post_meta( $post->ID, "turned_off", true );
-		}
-        
-		wp_send_json_success( [ "message" => count( $posts) . " filters are turned off." ] );
-        
-	}
-    
-	/**
-	 * Used for the Overview page
-	 */
+      foreach ( $posts as $post ) {
+          update_post_meta( $post->ID, "turned_off", true );
+      }
+      
+      wp_send_json_success( [ "message" => count( $posts) . " filters are turned off." ] );
+      
+  }
+  
+  /**
+   * Used for the Overview page
+   */
   function po_mark_tab_complete(){
     
     $tab_id  = sanitize_textarea_field( $_POST["tab_id"] );
@@ -355,26 +359,26 @@ class SOSPO_Ajax {
         update_user_meta( $user_id, "completed_overview_tabs", $user_tabs_completed );
     }
       
-	  wp_send_json_success( [ "message" => "Completed tabs are now remembered." ] );
+      wp_send_json_success( [ "message" => "Completed tabs are now remembered." ] );
       
   }
-    
-	/**
-	 * From the Settings page
-	 */
+  
+  /**
+   * From the Settings page
+   */
   function po_save_option_alphabetize_menu(){
     
     $should  = $_POST["should_alphabetize"] === "true";
     
     update_option("po_should_alphabetize_menu", $should );
     
-	  wp_send_json_success( [ "message" => "Option saved successfully." ] );
+      wp_send_json_success( [ "message" => "Option saved successfully." ] );
       
   }
     
-	/**
-	 * From the Filters List page
-	 */
+  /**
+   * From the Filters List page
+   */
   function po_turn_off_filter(){
       
     $turned_off  = $_POST["turned_off"] === "true";
@@ -382,13 +386,13 @@ class SOSPO_Ajax {
     
     update_post_meta( $post_id, "turned_off", $turned_off );
       
-	  wp_send_json_success( [ "message" => "Filter turned " . ( $turned_off ? "off" : "on" ) . " successfully." ] );
+      wp_send_json_success( [ "message" => "Filter turned " . ( $turned_off ? "off" : "on" ) . " successfully." ] );
       
   }
     
-	/**
-	 * Save the admin menu sidebar HTML in the database
-	 */
+  /**
+   * Save the admin menu sidebar HTML in the database
+   */
   function po_save_original_menu(){
     
     $menu_html          = wp_kses_post( stripcslashes( $_POST["menu_html"]        ) );
@@ -401,13 +405,13 @@ class SOSPO_Ajax {
     update_option( "plgnoptmzr_topbar_menu",   $topbar_menu_html );
     update_option( "plgnoptmzr_new_posts",     $new_html );
       
-	  wp_send_json_success( [ "message" => "Menu saved successfully." ] );
+      wp_send_json_success( [ "message" => "Menu saved successfully." ] );
       
   }
-  
-	/**
-	 * Because Ajax is not being filtered, we can get a full list of post types
-	 */
+
+  /**
+   * Because Ajax is not being filtered, we can get a full list of post types
+   */
   function po_get_post_types(){
       
     $post_types = [];
@@ -433,7 +437,7 @@ class SOSPO_Ajax {
     
     natsort( $post_types );
       
-	  wp_send_json_success( [ "message" => "Post types fetched.", "post_types" => $post_types ] );
+      wp_send_json_success( [ "message" => "Post types fetched.", "post_types" => $post_types ] );
       
   }
   
@@ -476,7 +480,7 @@ class SOSPO_Ajax {
     
     update_user_meta( get_current_user_id(), "sospo_filter_columns", $data );
       
-	  wp_send_json_success( [ "message" => "All good, the columns are saved." ] );
+      wp_send_json_success( [ "message" => "All good, the columns are saved." ] );
 
   }
     
